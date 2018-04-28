856c28887aee77245f5519964e03af62b2a809af
Lali <nemeth.lali@gmail.com>
2018-04-28 22:04:28 +0300

    Project setup

---
+++ test/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 28, 2018
+ *      Author: lali
+ */
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "test for coding exercise"
+#define BOOST_AUTO_TEST_MAIN
+#include <boost/test/auto_unit_test.hpp>
+
+
