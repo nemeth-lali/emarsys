12d02b04a2569490b50f597d8bce1919fd4f1a6c
Lali <nemeth.lali@gmail.com>
2018-05-02 23:31:13 +0300

    More testcases

--- src/routePlanner.hpp
+++ src/routePlanner.hpp
@@ -10,18 +10,42 @@
  * This can be solved with other graph implementation but for now I do my own implementation
  * Problem:
  * No endpoint for the route
+ * x=>y
  * Decision:
- * The path accepted but in the printout marked with an NoEnd at the end
+ * The path ignored and dropped from output (all unterminated paths are dropped from the output x=>y rule ignored)
  *
  * Problem:
  * Path contains circle
+ * x=>y
+ * y=>x
  * Decision:
  * Spanning tree algorithm cuts off the circles
  *
  * Problem:
+ * Point is endpoint but also source of route
+ * x=>y
+ * x=>
+ * Decision: x used as endpoint x=>y rule ignored
+ *
+ *
+ * Problem:
  * Two paths leads to the same direction
+ * x=>y
+ * z=>y
+ * z=>
+ * Decision:both paths included in the result
+ *
+ * Problem:
+ * Two paths overlaps each other
+ * w=>x
+ * x=>y
+ * y=>z
+ * u=>x
+ * y=>s
+ * s=>
+ * z=>
  * Decision:
- * See above
+ * both paths are inluded in the result
  */
 #pragma once
 #include <string>
--- test/routePlanner_test.cpp
+++ test/routePlanner_test.cpp
@@ -95,6 +95,143 @@ BOOST_AUTO_TEST_CASE(Simple2Endpoints)
     BOOST_CHECK_EQUAL("xuyvwz", result);
 }
 
+/*
+ * This can be solved with other graph implementation but for now I do my own implementation
+ * Problem:
+ * No endpoint for the route
+ * x=>y
+ * Decision:
+ * The path ignored and dropped from output (all unterminated paths are dropped from the output x=>y rule ignored)
+ */
+BOOST_AUTO_TEST_CASE(NoEndPoint)
+{
+    std::stringstream ss;
+    RoutePlanner sut;
+    sut.addPoint("x", "y");
+    sut.printPath(ss);
+    sut.createSpanningTree();
+    std::string result; // no initialization required
+    ss >> result;
+    BOOST_CHECK_EQUAL("", result);
+}
+
+
+ /*
+ * Problem:
+ * Path contains circle
+ * x=>y
+ * y=>x
+ * Decision:
+ * Spanning tree algorithm cuts off the circles
+ */
+BOOST_AUTO_TEST_CASE(Circle)
+{
+    std::stringstream ss;
+    RoutePlanner sut;
+    sut.addPoint("x", "y");
+    sut.addPoint("y", "x");
+    sut.printPath(ss);
+    sut.createSpanningTree();
+    std::string result; // no initialization required
+    ss >> result;
+    BOOST_CHECK_EQUAL("", result);
+}
+
+ /* Problem:
+ * Point is endpoint but also source of route
+ * x=>y
+ * x=>
+ * Decision: x used as endpoint x=>y rule ignored
+ */
+BOOST_AUTO_TEST_CASE(EndpointContinues)
+{
+    std::stringstream ss;
+    RoutePlanner sut;
+    sut.addPoint("x", "y");
+    sut.addEndPoint("x");
+    sut.printPath(ss);
+    sut.createSpanningTree();
+    std::string result; // no initialization required
+    ss >> result;
+    BOOST_CHECK_EQUAL("x", result);
+}
+
+/* Problem:
+* Point is endpoint but also source of route which continues
+* x=>y
+* x=>
+* y=>z
+* z=>
+* Decision: x used as endpoint x=>y rule ignored
+*/
+BOOST_AUTO_TEST_CASE(EndpointContinues)
+{
+   std::stringstream ss;
+   RoutePlanner sut;
+   sut.addPoint("x", "y");
+   sut.addPoint("y", "z");
+   sut.addEndPoint("x");
+   sut.addEndPoint("z");
+   sut.printPath(ss);
+   sut.createSpanningTree();
+   std::string result; // no initialization required
+   ss >> result;
+   BOOST_CHECK_EQUAL("xyz", result);
+}
+
+ /*
+ * Problem:
+ * Two paths leads to the same direction
+ * x=>y
+ * z=>y
+ * z=>
+ * Decision:both paths included in the result
+ */
+BOOST_AUTO_TEST_CASE(TwoPathsSameDirection)
+{
+    std::stringstream ss;
+    RoutePlanner sut;
+    sut.addPoint("x", "y");
+    sut.addPoint("z", "y");
+    sut.addEndPoint("y");
+    sut.printPath(ss);
+    sut.createSpanningTree();
+    std::string result; // no initialization required
+    ss >> result;
+    BOOST_CHECK_EQUAL("xyzy", result);
+}
+
+ /* Problem:
+ * Two paths overlaps each other
+ * w=>x
+ * x=>y
+ * y=>z
+ * u=>x
+ * y=>s
+ * s=>
+ * z=>
+ * Decision:
+ * both paths are inluded in the result
+ */
+BOOST_AUTO_TEST_CASE(OverlappingPaths)
+{
+    std::stringstream ss;
+    RoutePlanner sut;
+    sut.addPoint("w", "x");
+    sut.addPoint("x", "y");
+    sut.addPoint("y", "z");
+    sut.addPoint("u", "x");
+    sut.addPoint("y", "s");
+    sut.addEndPoint("s");
+    sut.addEndPoint("z");
+    sut.printPath(ss);
+    sut.createSpanningTree();
+    std::string result; // no initialization required
+    ss >> result;
+    BOOST_CHECK_EQUAL("wxyzuxys", result);
+}
+
+
 
 BOOST_AUTO_TEST_SUITE_END( )
 
