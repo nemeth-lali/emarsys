6e1c756928125273d18625b7f8289a40c30c0464
Lali <nemeth.lali@gmail.com>
2018-05-12 08:51:15 +0200

    Move helper logic to function

--- src/dueDateCalculator.cpp
+++ src/dueDateCalculator.cpp
@@ -11,21 +11,34 @@
 namespace dueDateCalculator {
 
 static const int WORKING_HOURS=8;
+static const int MONDAY=1;
+static const int FRIDAY=5;
+static const int BEGINNING_OF_WORKDAY=9;
+static const int END_OF_WORKDAY=17;
+static const time_t ONE_DAY = 24 * 60 * 60 ;
+
 
 tm DueDateCalculator::calculateDueDate(const tm& submitDate, const float turnAroundTime)
 {
-    static const time_t ONE_DAY = 24 * 60 * 60 ;
-    if(submitDate.tm_wday >=6 || !(9 < submitDate.tm_hour && submitDate.tm_hour <17))
+    if(!isWorkingTime(submitDate))
     {
         tm retVal;
         ::memset(&retVal,-1, sizeof(retVal)); // fastest way to set all members to -1
         return retVal;
     }
 
+    //int extradays = turnAroundTime % WORKING_HOURS;
+    //float remainingHours = turnAroundTime / WORKING_HOURS;
+    //addDays(submitDate, extradays);
     tm retVal = submitDate;
     retVal.tm_hour += turnAroundTime;
     return retVal;
+}
 
+bool DueDateCalculator::isWorkingTime(const tm& submitDate)
+{
+    return (MONDAY <= submitDate.tm_wday && submitDate.tm_wday <=FRIDAY)  &&
+           (BEGINNING_OF_WORKDAY <= submitDate.tm_hour && submitDate.tm_hour <= END_OF_WORKDAY);
 }
 
 }
--- src/dueDateCalculator.hpp
+++ src/dueDateCalculator.hpp
@@ -12,6 +12,7 @@ namespace dueDateCalculator {
 
 /*
  *  Assumption: Submit date is submit date and time
+ *  Decision: Sunday is the first day of the week (Use the UK style not Hungarian)
  *  Your task is to implement the CalculateDueDate method, which takes the submit date and
  *  turnaround time as an input and returns the date and time when the issue is to be resolved.
  */
@@ -25,6 +26,8 @@ public:
      * @return The expected delivery time of the issue -1 in tm_hour if the time is out of working hours or day
      */
     static tm calculateDueDate(const tm& submitDate, const float turnAroundTime);
+private:
+    static bool isWorkingTime(const tm& submitDate);
 };
 
 } /* namespace dueDateCalculator */
